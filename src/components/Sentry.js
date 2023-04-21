4459bbede0fc2599f658d275080f2279f7df2ab5
Vladimir Martsul <vladimir.martsul@gmail.com>
2023-04-21 18:17:20 +0300

    Update Sentry integration
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- src/components/Sentry.js
+++ src/components/Sentry.js
@@ -1,7 +1,6 @@
 import * as Sentry from "@sentry/browser";
-import {BrowserTracing} from "@sentry/tracing";
 
 Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
-    integrations: [new BrowserTracing()],
+    integrations: [new Sentry.BrowserTracing()],
 });
